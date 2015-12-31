1c8e1fad0e801716069e8725d846ccf33ca904f5
uzaif313 <uzaif313@gmail.com>
2015-12-31 13:07:14 +0530

    Providing Simple Static Pages With Testing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,20 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Static Page Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Static Page  Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Static Page  Sample App"
   end
+
 
 end
