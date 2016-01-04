77e2d517c5ceaae6c3b85246ebcff3dfcc8bce1e
uzaif313 <uzaif313@gmail.com>
2016-01-04 15:57:25 +0530

    Singup with Validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,18 @@ class UsersController < ApplicationController
     @user=User.new
   end
 
+  def create
+    @user=User.new(user_params)
+    if @user.save
+
+    else
+        render "new"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 
 end
