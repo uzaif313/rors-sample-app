91241adab91af74cecf607cce907cbdcc1481834
uzaif313 <uzaif313@gmail.com>
2016-01-04 17:28:21 +0530

    Added Signup Page Valid and Invalid users test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
-
+      flash[:success]="Welcome to the Sample Apps"
+      redirect_to @user
     else
         render "new"
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
    end
    assert_template 'users/new'
  end
+
+ test "valid singup information" do
+   get signup_path
+   assert_difference "User.count",1 do
+     post_via_redirect users_path, user: { name:  "Example User",
+                                email: "user@example.com",
+                                password:              "password",
+                                password_confirmation: "password"
+                              }
+   end
+   assert_template "users/show"
+ end
 end
