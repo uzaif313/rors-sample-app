14e2f6f144e6fdba610c896cf0884c87f00e717b
uzaif313 <uzaif313@gmail.com>
2016-01-04 15:03:33 +0530

    Initial Commit on Sing-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user=User.find(params[:id])
+    # debugger
   end
+
+  def new
+    @user=User.new
+  end
+
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for user
+    g_id=user.email.downcase
+    g_url="https://secure.gravatar.com/avatar/#{g_id}"
+    image_tag g_url,alt:user.name,class:"gravatar"
+  end
 end
