d14a097f12bbe8e5552f6eed76faa0ba28bf3e02
uzaif313 <uzaif313@gmail.com>
2016-01-05 17:46:04 +0530

    Add Proper Authentication Model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :logged_in_user,only:[:index,:edit,:update]
+  before_action :correct_user,only:[:edit,:update]
   def show
     @user=User.find(params[:id])
     # debugger
@@ -20,6 +21,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user=User.find_by(:id=>params[:id]);
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success]="Profile Updated successfully"
+      redirect_to user_path
+    else
+      render "edit"
+    end
+  end
+
 
 
   private
@@ -27,4 +42,17 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
 
+    def logged_in_user
+        unless logged_in?
+          flash[:danger]="Access Denied ! Please Try This Action With Login"
+          redirect_to login_url
+        end
+    end
+
+    def correct_user
+      user=User.find(params[:id])
+      unless user==current_user
+        redirect_to root_url
+      end
+    end
 end
