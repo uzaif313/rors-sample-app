0b85ed98461a9ec4e949572a6351857be6d44cf5
uzaif313 <uzaif313@gmail.com>
2016-01-05 17:47:17 +0530

    Add Proper Authentication Model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
   before_action :logged_in_user,only:[:index,:edit,:update]
   before_action :correct_user,only:[:edit,:update]
   def show
