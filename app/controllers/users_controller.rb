5dd9a020b932d73f9381539bc32e274d244f5085
uzaif313 <uzaif313@gmail.com>
2016-01-05 18:20:29 +0530

    Add Update Functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
   before_action :logged_in_user,only:[:index,:edit,:update]
   before_action :correct_user,only:[:edit,:update]
+
+  def index
+    @users = User.all
+  end
+
   def show
     @user=User.find(params[:id])
     # debugger
@@ -44,7 +49,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
         unless logged_in?
-          flash[:danger]="Access Denied ! Please Try This Action With Login"
+          flash[:danger]="Access Denied  Please Login and View the Page"
           redirect_to login_url
         end
     end
