80bf03089ba4248f247c40602ad8bc872c37abdd
uzaif313 <uzaif313@gmail.com>
2016-01-07 13:45:37 +0530

    Modify prodction file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
    :address        => 'smtp.sendgrid.net',
    :port           => '587',
    :authentication => :plain,
-   :user_name      => 'uzaif',
-   :password       => '**uzef*1993**',
+   :user_name      => '',
+   :password       => '',
    :domain         => 'heroku.com',
    :enable_starttls_auto => true
  }
