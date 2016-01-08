d2893e1870ab82237462663abc46ce235adfa788
uzaif313 <uzaif313@gmail.com>
2016-01-08 10:13:04 +0530

    Desc Ordering Post Listing on Home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost=current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page],per_page:5)
+      @feed_items = current_user.feed.paginate(page: params[:page],per_page:5).order('created_at DESC')
     end
   end
 
